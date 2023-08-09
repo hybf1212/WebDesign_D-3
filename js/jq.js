649b3457ed38868582f5996a769a9fa9c160f6e4
hybf1212 <hybf1212@naver.com>
2023-08-09 21:55:58 +0900

    공지사항 갤러리

--- js/jq.js
+++ js/jq.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
         count %= 3
         $(".slide>div").eq(count).addClass("on").siblings().removeClass("on")
     },3000)
+
+    $(".notice>a").click(function(){
+        $(".notice").addClass("active")
+        $(".gallery").removeClass("active")
+    })
+    $(".gallery>a").click(function(){
+        $(".gallery").addClass("active")
+        $(".notice").removeClass("active")
+    })
 })
